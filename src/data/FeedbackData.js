441729cf8952993a5bebf355bac693adefd0f7c5
hhs <husahin_0629@outlook.com>
2022-02-08 23:48:04 +0300

    initial

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,18 +1,18 @@
 const FeedbackData = [
   {
     id: 1,
-    rating: 8,
-    review: 'This is feedback 1',
+    rating: 10,
+    review: 'I learned a lot and it was explained clear and detailed. I am excited especially in challenging section because I apply what ever I learned on each topic. I recommend this tutorial to those who want to learn Web development. Thank you and more power.',
   },
   {
     id: 2,
-    rating: 6,
-    review: 'This is feedback 2',
+    rating: 3,
+    review: 'This course is outdated and, excluding the first modules regarding css and html, very superficial. Not recommended.',
   },
   {
     id: 3,
-    rating: 10,
-    review: 'This is feedback 3',
+    rating: 7,
+    review: 'Well, this is a really good course. The instructor is also really good. The frontend part of the course is amazing but when it comes to the latter part of the course, some course content is outdated. That s why I am giving 7 stars',
   },
 ]
 
